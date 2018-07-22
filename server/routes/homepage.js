d3f24d96c69987950b47f1f7a9d01d25cf30936a
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-22 22:11:26 +0100

    Add mainSlider route

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get('/',(req,res)=>(
     res.render('homepage.hbs')
 ))
+router.get('/main-slider',(req,res)=>(
+    res.render('menu.hbs',{title:'Main Slider',linkTitle:'main-slider'})
+))
 
 module.exports = router;
